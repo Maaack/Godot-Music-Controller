2f675972ccbe9c4fe6c6fa90203a6e9e6d655cc7
Marek Belski <marek.belski@gmail.com>
2024-12-18 11:18:34 -0800

    Uses snake_case for files for OS compatibility.

--- addons/maaacks_music_controller/maaacks_music_controller.gd
+++ addons/maaacks_music_controller/maaacks_music_controller.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_autoload_singleton("ProjectMusicController", "res://addons/maaacks_music_controller/base/scenes/Autoloads/ProjectMusicController.tscn")
+	add_autoload_singleton("ProjectMusicController", "res://addons/maaacks_music_controller/base/scenes/autoloads/project_music_controller.tscn")
 
 func _exit_tree():
 	remove_autoload_singleton("ProjectMusicController")
