d3edd8abf869132854bb5d8b35ff73a145f9033b
Marek Belski <marek.belski@gmail.com>
2025-06-26 21:41:02 -0700

    Rsync with menus template.

--- addons/maaacks_music_controller/ATTRIBUTION.md
+++ addons/maaacks_music_controller/ATTRIBUTION.md
@@ -2,15 +2,17 @@
 ## Collaborators
 
 ### Godot Music Controller
-Author: [Marek Belski](https://github.com/Maaack/Godot-Music-Controller/graphs/contributors)  
+![Maaack Plugin Icon](/addons/maaacks_music_controller/assets/plugin_logo/logo.png)  
+Author: [Marek Belski and contributors](https://github.com/Maaack/Godot-Music-Controller/graphs/contributors)  
 Source: [github: Godot-Music-Controller](https://github.com/Maaack/Godot-Music-Controller)  
 License: [MIT License](LICENSE.txt)  
 
 ## Tools
 #### Godot
+![Godot Engine Logo](/addons/maaacks_music_controller/assets/godot_engine_logo/logo_vertical_color_dark.png)  
 Author: [Juan Linietsky, Ariel Manzur, and contributors](https://godotengine.org/contact)  
 Source: [godotengine.org](https://godotengine.org/)  
-License: [MIT License](https://github.com/godotengine/godot/blob/master/LICENSE.txt) 
+License: [MIT License](https://github.com/godotengine/godot/blob/master/LICENSE.txt)  
 
 #### Visual Studio Code
 Author: [Microsoft](https://opensource.microsoft.com/)  
@@ -18,6 +20,7 @@ Source: [github: vscode](https://github.com/microsoft/vscode)
 License: [MIT License](https://github.com/microsoft/vscode/blob/main/LICENSE.txt)
 
 #### Git
+![Git Logo](/addons/maaacks_music_controller/assets/git_logo/Git-Logo-2Color.png)  
 Author: [Linus Torvalds](https://github.com/torvalds)  
 Source: [git-scm.com](https://git-scm.com/downloads)  
 License: [GNU General Public License version 2](https://opensource.org/licenses/GPL-2.0)
--- addons/maaacks_music_controller/maaacks_music_controller.gd
+++ addons/maaacks_music_controller/maaacks_music_controller.gd
@@ -1,9 +1,50 @@
 @tool
 extends EditorPlugin
 
+const PLUGIN_NAME = "Maaack's Music Controller"
+const PROJECT_SETTINGS_PATH = "maaacks_music_controller/"
 
-func _enter_tree():
-	add_autoload_singleton("ProjectMusicController", "res://addons/maaacks_music_controller/base/scenes/autoloads/project_music_controller.tscn")
+var update_plugin_tool_string : String
 
-func _exit_tree():
+func _get_plugin_name() -> String:
+	return PLUGIN_NAME
+
+func get_plugin_path() -> String:
+	return get_script().resource_path.get_base_dir() + "/"
+
+func _open_check_plugin_version() -> void:
+	if ProjectSettings.has_setting(PROJECT_SETTINGS_PATH + "disable_update_check"):
+		if ProjectSettings.get_setting(PROJECT_SETTINGS_PATH + "disable_update_check"):
+			return
+	else:
+		ProjectSettings.set_setting(PROJECT_SETTINGS_PATH + "disable_update_check", false)
+		ProjectSettings.save()
+	var check_version_scene : PackedScene = load(get_plugin_path() + "installer/check_plugin_version.tscn")
+	var check_version_instance : Node = check_version_scene.instantiate()
+	check_version_instance.auto_start = true
+	check_version_instance.new_version_detected.connect(_add_update_plugin_tool_option)
+	add_child(check_version_instance)
+
+func _open_update_plugin() -> void:
+	var update_plugin_scene : PackedScene = load(get_plugin_path() + "installer/update_plugin.tscn")
+	var update_plugin_instance : Node = update_plugin_scene.instantiate()
+	update_plugin_instance.auto_start = true
+	update_plugin_instance.update_completed.connect(_remove_update_plugin_tool_option)
+	add_child(update_plugin_instance)
+
+func _add_update_plugin_tool_option(new_version : String) -> void:
+	update_plugin_tool_string = "Update %s to v%s..." % [_get_plugin_name(), new_version]
+	add_tool_menu_item(update_plugin_tool_string, _open_update_plugin)
+
+func _remove_update_plugin_tool_option() -> void:
+	if update_plugin_tool_string.is_empty(): return
+	remove_tool_menu_item(update_plugin_tool_string)
+	update_plugin_tool_string = ""
+
+func _enter_tree() -> void:
+	add_autoload_singleton("ProjectMusicController", get_plugin_path() + "base/scenes/autoloads/project_music_controller.tscn")
+	_open_check_plugin_version()
+
+func _exit_tree() -> void:
 	remove_autoload_singleton("ProjectMusicController")
+	_remove_update_plugin_tool_option()
